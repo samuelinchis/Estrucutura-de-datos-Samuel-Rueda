48431cc96e0117ec776c8b4a35b58d5901ef952e
Samuel Rueda Sánchez <110745715+samuelinchis@users.noreply.github.com>
2023-04-19 23:05:21 -0500

    complejidad
    Se añade la complejidad del punto 3 B

--- tarea5/punto3/b/lista.cpp
+++ tarea5/punto3/b/lista.cpp
@@ -4,23 +4,23 @@ Lista::Lista(){
   act = NULL;
 }
 
-void Lista::insListaOrdenada(Elemento elem){
-  Nodo* tmp = act;
-  Nodo* nuevo = new Nodo;
-  nuevo->dato = elem;
-  nuevo->sig = NULL;
+void Lista::insListaOrdenada(Elemento elem){ // O(n) 
+  Nodo* tmp = act;			// 1
+  Nodo* nuevo = new Nodo;		// 1
+  nuevo->dato = elem;			// 1
+  nuevo->sig = NULL;			// 1
 
-  if(act == NULL)
-    act = nuevo;
-  else if(nuevo->dato < tmp->dato){
-    nuevo->sig = tmp;
-    act = nuevo;
+  if(act == NULL)			// 1
+    act = nuevo;			// 1
+  else if(nuevo->dato < tmp->dato){	// 1
+    nuevo->sig = tmp;			// 1
+    act = nuevo;			// 1
   }
-  else{
-    while(tmp->sig != NULL && nuevo->dato > tmp->sig->dato)
-      tmp = tmp->sig;
-    nuevo->sig = tmp->sig;
-    tmp->sig = nuevo;
+  else{					// 1
+    while(tmp->sig != NULL && nuevo->dato > tmp->sig->dato) //n En el peor caso toca recorrer la lista entera 
+      tmp = tmp->sig;			// n-1
+    nuevo->sig = tmp->sig;		// 1
+    tmp->sig = nuevo;			// 1
   }
 }
 
